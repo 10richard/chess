16524de2bad9fe15a545718b24c768bf9448cc04
Richard Ten <rten04@icloud.com>
2023-03-26 19:57:31 -0400

    Add comments

--- lib/board.rb
+++ lib/board.rb
@@ -8,9 +8,13 @@ class Board
 
     def generate_board
         board = {}
+        #iterate through letters a to h
         for letter in 'a'..'h' do
+            #set letter's value to another hash
             board[letter] = {}
+            #iterate through numbers 1 to 8
             for num in '1'..'8' do
+                #inside of the letter's hash, set number as key, and piece as value
                 board[letter][num] = set_initial_pos(letter, num.to_i)
             end
         end
