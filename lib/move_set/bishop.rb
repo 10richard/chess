aae9c86dbf7d6bdd6bafe6ee29c24d13103b1dd2
Richard Ten <rten04@icloud.com>
2023-04-12 23:29:26 -0400

    Create methods to find valid moves
    **Extremely messy**
    Need to condense (via blocks?)
    
    Checks all diagonal directions of a bishop
    Appends position to "@valid_moves" if it is empty
    Once position is not empty, the methods will check if the piece is
    capturable - (if the piece belongs to the opposing set of pieces)

--- lib/move_set/bishop.rb
+++ lib/move_set/bishop.rb
@@ -3,16 +3,67 @@ class Bishop < Piece
     def find_valid_moves
 
     end
-    
-    def find_valid_captures
 
+
+    #find diagonal moves based on quadrants? (1, 2, 3, 4)
+    def diagonal_moves_quadrant_one
+        letter = @initial_pos[0]
+        num = @initial_pos[1]
+        capturable = white? ? @@black_pieces : @@white_pieces
+
+        while @letters.include?(letter) && @nums.include?(num) && @board[letter][num] == '-'
+            #gets previous letter in alphabet
+            letter = (letter.ord - 1).chr
+            num = num.to_i
+            num -= 1
+            num = num.to_s
+            @valid_moves.push(letter + num) if !@letters.include?(letter) || !@nums.include?(num)
+        end
+        @valid_moves.pop unless capturable.include?(@board[letter][num])
     end
 
-    def set_valid_moves
-        
+    def diagonal_moves_quadrant_two
+        letter = @initial_pos[0]
+        num = @initial_pos[1]
+        capturable = white? ? @@black_pieces : @@white_pieces
+
+        while @letters.include?(letter) && @nums.include?(num) && @board[letter][num] == '-'
+            #gets previous letter in alphabet
+            letter = (letter.ord + 1).chr
+            num = num.to_i
+            num -= 1
+            num = num.to_s
+            @valid_moves.push(letter + num) if !@letters.include?(letter) || !@nums.include?(num)
+        end
+        @valid_moves.pop unless capturable.include?(@board[letter][num])
     end
 
-    def set_valid_captures
+    def diagonal_moves_quadrant_three
+        letter = @initial_pos[0]
+        num = @initial_pos[1]
+        capturable = white? ? @@black_pieces : @@white_pieces
 
+        while @letters.include?(letter) && @nums.include?(num) && @board[letter][num] == '-'
+            #gets next letter in alphabet
+            letter = (letter.ord + 1).chr
+            num.next!
+            @valid_moves.push(letter + num) if !@letters.include?(letter) || !@nums.include?(num)
+        end
+        @valid_moves.pop unless capturable.include?(@board[letter][num])
+    end
+
+    #quadrant 4
+    def diagonal_moves_quadrant_four
+        letter = @initial_pos[0]
+        num = @initial_pos[1]
+        capturable = white? ? @@black_pieces : @@white_pieces
+
+        while @letters.include?(letter) && @nums.include?(num) && @board[letter][num] == '-'
+            #gets previous letter in alphabet
+            letter = (letter.ord - 1).chr
+            num.next!
+            @valid_moves.push(letter + num) if !@letters.include?(letter) || !@nums.include?(num)
+        end
+        @valid_moves.pop unless capturable.include?(@board[letter][num])
     end
 end
