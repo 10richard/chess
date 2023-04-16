f865bddfaa77aeff05d5bca1e3857044749b1448
Richard Ten <rten04@icloud.com>
2023-04-16 17:41:46 -0400

    Create a method to find valid moves of a knight
    **No clue if it works, need to test**
    
    flower_pattern method checks for all the moves of a knight using
    a "transformations" list

--- lib/move_set/knight.rb
+++ lib/move_set/knight.rb
@@ -1,3 +1,25 @@
 class Knight < Piece
 
+    def initialize
+        @@transformations = [[-2, -1], [-1, -2], [-2, 1], [-1, 2], 
+        [1, 2], [2, 1], [2, -1], [1, -2]].freeze
+    end
+
+    def find_valid_moves
+        flower_pattern
+    end
+
+    def flower_pattern
+        @@transformations.each do |t|
+            letter = @initial_pos[0]
+            num = @initial_pos[1].to_i
+            letter = (letter.ord + t[0]).chr
+            num = (num + t[1]).to_s
+            @valid_moves.push(letter + num.to_s) if on_board?(letter) && on_board?(num)
+        end
+    end
+
+    def on_board?(part)
+        @nums.include?(part) || @letters.include?(part)
+    end
 end
