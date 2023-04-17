d57abaf16ce63101af7caa89c89f2cb36a6b87c4
Richard Ten <rten04@icloud.com>
2023-04-17 12:25:48 -0400

    Create layout
    Added possible methods to use for checking validity of next moves

--- lib/move_set/king.rb
+++ lib/move_set/king.rb
@@ -1,7 +1,23 @@
 class King < Piece
-    
-    def initialize
-        @in_check = false
 
+    def find_valid_moves
+        #king can move in any direction, but only 1 square at a time
+        #check if king will be in_check if the king moves there
+    end
+    
+    def able_to_castle?
+        
+    end
+
+    def check_threats
+        #will check for all diagonals and horizontals
+        #every new_valid position, iterate through the above
+        
+        #there will have to be a special case for knights
+    end
+
+    def identify_threat
+        #if piece of opposing color is found, then this method will be called
+        #if piece matches (ie/ bishop at diagonal) then @valid_moves.pop. Otherwise do nothing
     end
 end
