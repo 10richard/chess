8c5fc80f17b64b14932e5c5b02e482d9589562f5
Richard Ten <rten04@icloud.com>
2023-04-19 19:54:47 -0400

    Make "on_board?" method inheritable

--- lib/move_set/piece.rb
+++ lib/move_set/piece.rb
@@ -1,16 +1,25 @@
 class Piece 
 
-    attr_accessor :position
+    attr_reader :valid_moves
+
+    @@white_pieces = ['♟︎', '♞', '♝', '♜', '♛', '♚']
+    @@black_pieces = ['♙', '♘', '♗', '♖', '♕', '♔']
 
     def initialize(board, initial_pos, color)
         @board = board
-        @initial_position = initial_pos
+        @initial_position = initial_pos #will be split (ie. ['a', '1'])
         @current = color
+        @letters = ('a'..'h').to_a
+        @nums = ('1'..'8').to_a
         @valid_moves = []
-        @valid_captures = []
     end
 
     def white?
         @current == 'white'
+    end
+
+    def on_board?(pos)
+        #check if position is on the board (pos = letter + num; ie. 'a1')
+        @letters.include?(pos[0]) && @nums.include?(pos[1])
     end
 end
