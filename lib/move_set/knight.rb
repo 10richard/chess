463219f1271749facd2b55629ece79a1bafc2886
Richard Ten <rten04@icloud.com>
2023-04-19 19:59:33 -0400

    Delete on_board? method

--- lib/move_set/knight.rb
+++ lib/move_set/knight.rb
@@ -15,12 +15,8 @@ class Knight < Piece
             num = @initial_pos[1].to_i
             letter = (letter.ord + t[0]).chr
             num = (num + t[1]).to_s
-            @valid_moves.push(letter + num.to_s) if on_board?(letter) && on_board?(num) && position_capturable?(letter + num)
+            @valid_moves.push(letter + num.to_s) if on_board?(letter + num) && position_capturable?(letter + num)
         end
-    end
-
-    def on_board?(part)
-        @nums.include?(part) || @letters.include?(part)
     end
 
     def position_capturable?(pos)
