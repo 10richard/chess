38e79eeadbb1c6986f78ade770f71b1a62d32747
Richard Ten <rten04@icloud.com>
2023-04-21 00:37:59 -0400

    Fix class and add new method

--- lib/move_set/pawn.rb
+++ lib/move_set/pawn.rb
@@ -1,18 +1,29 @@
+require_relative './piece.rb'
+
 class Pawn < Piece
-    
+
     def find_valid_moves
         letter = @initial_pos[0]
         num = @initial_pos[1]
         color = white? ? 'white' : 'black'
         if on_start_pos?(num, color)
             #change the bottom loop l8
-            for n in 1..2
-                @valid_moves.push(letter + num.next)
+            if color == 'white'
+                for n in 1..2
+                    num = num.next
+                    @valid_moves.push(letter + num) if @board.board[letter][num] == '-'
+                end
+            else
+                for n in 1..2
+                    num = (num.to_i - 1).to_s
+                    @valid_moves.push(letter + num) if @board.board[letter][num] == '-'
+                end
             end
-        elsif @board[letter][num.next] == '-'
+        elsif @board.board[letter][num.next] == '-'
             @valid_moves.push(letter + num.next)
         end
-        able_to_capture?(color)
+        able_to_capture?
+        @valid_moves
     end
 
     def on_start_pos?(num, color)
@@ -22,17 +33,23 @@ class Pawn < Piece
         false
     end
 
-    def able_to_capture?(color)
-        transformations = [[-1, -1]. [1, 1]]
+    def able_to_capture?
+        transformations = white? ? [[-1, 1], [1, 1]] : [[1, -1], [-1, -1]]
         #find if pawn can capture any pieces and append to valid moves if yes
         capturable = white? ? @@black_pieces : @@white_pieces
-
         transformations.each do |t|
             letter = @initial_pos[0]
             num = @initial_pos[1]
             letter = (letter.ord + t[0]).chr
-            num = (letter.ord + t[1]).chr
-            @valid_moves.push(letter + num) if capturable.include?(@board[letter][num])
+            num = (num.ord + t[1]).chr
+            puts letter + num
+            @valid_moves.push(letter + num) if on_board?(letter + num) && capturable.include?(@board.board[letter][num])
         end
+    end
+
+    def able_to_transform?
+        #if black pawn reach number 1 then true
+        #if white pawn reach number 8 then true
+        #can transform to queen, rook, bishop, or knight
     end
 end
