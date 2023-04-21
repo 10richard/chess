d8c3ccf49cb311d00d0d2242f527bc8623935b2d
Richard Ten <rten04@icloud.com>
2023-04-21 00:46:51 -0400

    Fix on_board? method

--- lib/move_set/piece.rb
+++ lib/move_set/piece.rb
@@ -1,13 +1,13 @@
 class Piece 
 
-    attr_reader :valid_moves
+    attr_reader :valid_moves, :board
 
     @@white_pieces = ['♟︎', '♞', '♝', '♜', '♛', '♚']
     @@black_pieces = ['♙', '♘', '♗', '♖', '♕', '♔']
 
     def initialize(board, initial_pos, color)
         @board = board
-        @initial_position = initial_pos #will be split (ie. ['a', '1'])
+        @initial_pos = initial_pos #will be split (ie. ['a', '1'])
         @current = color
         @letters = ('a'..'h').to_a
         @nums = ('1'..'8').to_a
@@ -20,6 +20,6 @@ class Piece
 
     def on_board?(pos)
         #check if position is on the board (pos = letter + num; ie. 'a1')
-        @letters.include?(pos[0]) && @nums.include?(pos[1])
+        @letters.include?(pos[0]) && @nums.include?(pos[1]) && pos.size == 2
     end
 end
