cdaf7f5a4e28006ddc806d9654401016dca5ec73
Richard Ten <rten04@icloud.com>
2023-04-21 00:47:29 -0400

    Finish/fix some methods
    Finished valid_new_pos?
    Checks if new position is included in all the possible moves of the selected piece
    
    Methods use a loop instead of recursion when validating initial/new positions

--- lib/game.rb
+++ lib/game.rb
@@ -7,14 +7,16 @@ class Game < Board
     include Display
 
     attr_accessor :board
-    attr_reader :current
+    attr_reader :current, :initial_pos
 
     def initialize
         @board = Board.new()
         #interchange current with 'white' and 'black'
         @current = 'white'
         @game_over = nil
-        @piece_selected = ''
+        @got_pos = false
+        @initial_pos = ''
+        @new_pos = ''
         #if king is in check, call another method (other than select_old_pos)
         #ie/ move_king
     end
@@ -23,18 +25,23 @@ class Game < Board
         until @game_over
             display_turn(@current)
             display_board(@board.board, @current)
-            initial_pos = select_initial_pos
-            new_pos = select_new_pos
-            board.modify_board(initial_pos, new_pos)
+            @initial_pos = select_initial_pos
+            @got_pos = false
+            @new_pos = select_new_pos
+            @got_pos = false
+            board.modify_board(@initial_pos, @new_pos)
             @current == 'white' ? @current = 'black' : @current = 'white'
-            @game_over = 'yes'
         end
+        #check @game_over var
     end
 
     def select_initial_pos
-        puts get_player_move('initial_pos')
-        initial_pos = gets.chomp
-        valid_old_pos?(initial_pos.split(''))
+        until @got_pos
+            puts get_player_move('initial_pos')
+            initial_pos = gets.chomp.downcase
+            @got_pos = valid_old_pos?(initial_pos.split(''))
+        end
+        initial_pos
     end
 
     def valid_old_pos?(split_pos)
@@ -44,29 +51,44 @@ class Game < Board
         #if old_pos does not match current_player's color, then callback select_old_pos
         if !position_on_board?(split_pos)
             puts selection_error('invalid_pos')
-            select_initial_pos
-        elsif board.get_piece(split_pos) == ' '
+            return false
+        elsif board.get_piece(split_pos) == '-'
             puts selection_error('empty_pos')
-            select_initial_pos
+            return false
         elsif board.get_piece_color(split_pos) != @current
             puts selection_error('invalid_color', @current)
-            select_initial_pos
+            return false
         end
         puts confirm_move('initial_pos', board.get_piece(split_pos), split_pos.join(''))
+        true
     end
 
     def select_new_pos
-        puts get_player_move('new_pos')
-        new_pos = gets.chomp
-        valid_new_pos?(new_pos)
+        until @got_pos
+            puts get_player_move('new_pos')
+            new_pos = gets.chomp.downcase
+            @got_pos = valid_new_pos?(new_pos)
+        end
+        new_pos
     end
 
-    def valid_new_pos?
+    def valid_new_pos?(new_pos)
+        #check if new_pos is not off the board
         #check if new_pos is in valid_moves of the piece (Piece class)
         #check if piece is able to move into selected position (if new position is empty or opposing color is on it)
+        old_pos = @initial_pos
+        piece = @board.get_piece(@initial_pos)
+        color = @board.get_piece_color(@initial_pos)
+        possible_moves = valid_moves?(old_pos, piece, color, @board)
+        p possible_moves
         
-        #if unable to/there are pieces in the way, then throw an error and call select_new_pos again
-        #else return new_pos
+        if possible_moves.include?(new_pos)
+            p confirm_move('new_pos', piece, new_pos)
+            return true
+        else
+            p selection_error('invalid_new_pos')
+            return false
+        end
     end
 
     def king_in_check?
