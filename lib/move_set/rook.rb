9242fc636c47b639a192838dc72bf51d5a205f7c
Richard Ten <rten04@icloud.com>
2023-04-21 20:41:17 -0400

    Fix horizontal and vertical methods
    Modified similarly to the bishops methods

--- lib/move_set/rook.rb
+++ lib/move_set/rook.rb
@@ -1,8 +1,11 @@
+require_relative './piece.rb'
+
 class Rook < Piece
 
     def find_valid_moves
         horizontal
         vertical
+        @valid_moves
     end
 
     def horizontal
@@ -10,21 +13,25 @@ class Rook < Piece
         num = @initial_pos[1]
         capturable = white? ? @@black_pieces : @@white_pieces
 
-        while @letters.include?(letter) && @board[letter][num] == '-'
-            #gets previous letter in alphabet
+        loop do
+            #gets next letter in alphabet
             letter = (letter.ord - 1).chr
-            @valid_moves.push(letter + num) if @letters.include?(letter)
+            @valid_moves.push(letter + num)
+
+            break unless @letters.include?(letter) && @board.board[letter][num] == '-'
         end
-        @valid_moves.pop unless capturable.include?(@board[letter][num])
+        @valid_moves.pop unless on_board?(letter + num) && capturable.include?(@board.board[letter][num])
 
         letter = @initial_pos[0]
 
-        while @letters.include?(letter) && @board[letter][num] == '-'
-            #gets previous letter in alphabet
+        loop do
+            #gets next letter in alphabet
             letter = (letter.ord + 1).chr
-            @valid_moves.push(letter + num) if @letters.include?(letter)
+            @valid_moves.push(letter + num)
+
+            break unless @letters.include?(letter) && @board.board[letter][num] == '-'
         end
-        @valid_moves.pop unless capturable.include?(@board[letter][num])
+        @valid_moves.pop unless on_board?(letter + num) && capturable.include?(@board.board[letter][num])
     end
 
     def vertical
@@ -32,24 +39,28 @@ class Rook < Piece
         num = @initial_pos[1]
         capturable = white? ? @@black_pieces : @@white_pieces
 
-        while @nums.include?(num) && @board[letter][num] == '-'
-            #gets previous letter in alphabet
+        loop do
+            #gets next letter in alphabet
             num = num.to_i
             num -= 1
             num = num.to_s
-            @valid_moves.push(letter + num) if @nums.include?(num)
+            @valid_moves.push(letter + num)
+
+            break unless @nums.include?(num) && @board.board[letter][num] == '-'
         end
-        @valid_moves.pop unless capturable.include?(@board[letter][num])
+        @valid_moves.pop unless on_board?(letter + num) && capturable.include?(@board.board[letter][num])
 
         num = @initial_pos[1]
 
-        while @nums.include?(num) && @board[letter][num] == '-'
-            #gets previous letter in alphabet
+        loop do
+            #gets next letter in alphabet
             num = num.to_i
             num += 1
             num = num.to_s
-            @valid_moves.push(letter + num) if @nums.include?(num)
+            @valid_moves.push(letter + num)
+
+            break unless @nums.include?(num) && @board.board[letter][num] == '-'
         end
-        @valid_moves.pop unless capturable.include?(@board[letter][num])
+        @valid_moves.pop unless on_board?(letter + num) && capturable.include?(@board.board[letter][num])
     end
 end
